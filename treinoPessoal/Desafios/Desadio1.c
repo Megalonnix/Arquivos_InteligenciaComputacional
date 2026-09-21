382e1499805478397a11adf2ae726b0131c617d2
Megalonnix <149280452+Megalonnix@users.noreply.github.com>
2026-09-21 16:18:27 -0300

    Desafio 1 criado

--- treinoPessoal/Desafios/Desadio1.c
+++ treinoPessoal/Desafios/Desadio1.c
@@ -23,9 +23,10 @@ int main() {
     printf("Digite vl2: ");
     scanf("%d", &vl2);
 
-    printf("(vl1) = %d | (vl2) = %d", vl1, vl2);
+    printf("\n[vl1] = %d | [vl2] = %d", vl1, vl2);
 
-    printf("\n\n=== TROCAR VALORES: ===\n    [vl1] <-> [vl2]\n\n");
+    printf("\n\n=== TROCAR VALORES: ===\n\n");
+    printf("=== [vl1] <-> [vl2] ===\n\n");
 
     int *pont_vl1 = &vl1;
     int *pont_vl2 = &vl2;
@@ -35,7 +36,7 @@ int main() {
     vl1 = *pont_vl2;
     vl2 = aux;
 
-    printf("(vl1) = %d | (vl2) = %d \n\n", vl1, vl2);
+    printf("[vl1] = %d | [vl2] = %d \n\n", vl1, vl2);
 
     return 0;
 }
