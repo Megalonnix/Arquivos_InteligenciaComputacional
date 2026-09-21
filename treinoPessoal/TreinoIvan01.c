281ddd97ed4c95d4837bd15f6b260da769b4b1c9
Megalonnix <149280452+Megalonnix@users.noreply.github.com>
2026-09-21 09:26:19 -0300

    Entendendo melhor nuances

--- treinoPessoal/TreinoIvan01.c
+++ treinoPessoal/TreinoIvan01.c
@@ -13,9 +13,9 @@
 # include <stdio.h>
 
 int main() {
-    int x = 27;
+    int x = 1000;
 
-    printf("=== 1. O QUE É O '&' (ENDERECO) ===\n");
+    printf("=== 1. O QUE E O '&' (ENDERECO) ===\n\n");
     // 'x' é o VALOR dentro da caixa.
     // '&x' é a LOCALIZAÇÃO  (endereço) da caixa na memoria RAM.
     printf("Valor de x: %d\n", x);
@@ -27,7 +27,7 @@ int main() {
     // passamos &x. Se passásemos apenas 'x', o scanf receberia 
     // o número 10 e tentaria gravar  no endereço de memória 10!!!
     // (o que causaria um  CRASH/trava) :P
-    printf("=== 2. PASSANDO O '&' PARA ALTERAR VALORES (SCANF) ===\n");
+    printf("=== 2. PASSANDO O '&' PARA ALTERAR VALORES (SCANF) ===\n\n");
     printf("Digite um novo valor para x: ");
     scanf("%d",&x);
 
@@ -35,4 +35,11 @@ int main() {
                                             // caso vc adicione &+x (&x) o output será um endereço aleatório!
 
     printf("O endereco continua sendo o mesmo: %p", &x);
+
+    printf("\n\n=== 3. INTRODUCAO A PONTEIROS (VARIAVEL QUE GUARDA ENDERECO) ===\n\n");
+    // Um ponteiro 'ptr' guarda o endereço retornado pelo '&'
+    int *ponteiro1 = &x;
+    printf("Valor de endereco do PONTEIRO1 (que e o endereco de x):\n");
+    printf("\nValor PONTEIRO1: %p", ponteiro1);
+    printf("\nValor ONDE o PONTEIRO1 aponta (usando *ponteiro1): %d \n", *ponteiro1);
 }
